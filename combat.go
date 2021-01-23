a5e19549d0641af4a45891a9527843e89407f2b9
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2021-01-22 21:49:54 -0500

    Add correct validWeapons list and name all weapon IDs

--- combat.go
+++ combat.go
@@ -1,7 +1,56 @@
 package main
 
 var (
-	validWeapons = []int{1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 14, 15, 16, 20, 40}
+	validWeapons = []int{
+		0, 6, 61, 16, 31, 1,
+		60, 32, 5, 62, 10, 20,
+		3, 12, 22, 24, 37, 25,
+		26, 33, 64, 38, 39, 35,
+		36, 40, 2, 4, 63, 21,
+		14, 7, 41, 27, 19, 23,
+	}
+)
+
+//Weapon is the weapon ID of a weapon
+type Weapon int
+
+const (
+	weaponPistol          Weapon = 0
+	weaponRevolver        Weapon = 6
+	weaponDeagle          Weapon = 61
+	weaponUzi             Weapon = 16
+	weaponGodPistol       Weapon = 31
+	weaponAK47            Weapon = 1
+	weaponM16             Weapon = 60
+	weaponM1              Weapon = 32
+	weaponSniper          Weapon = 5
+	weaponSawedOff        Weapon = 62
+	weaponMilitaryShotgun Weapon = 10
+	weaponBouncer         Weapon = 20
+	weaponGrenadeLauncher Weapon = 3
+	weaponThruster        Weapon = 12
+	weaponRPG             Weapon = 22
+	weaponSnakePistol     Weapon = 24
+	weaponSnakeShotgun    Weapon = 37
+	weaponSnakeGrenade    Weapon = 25
+	weaponSnakeLauncher   Weapon = 26
+	weaponSnakeMinigun    Weapon = 33
+	weaponFlyingSnake     Weapon = 64
+	weaponSpikeBall       Weapon = 38
+	weaponLavaBeam        Weapon = 39
+	weaponLavaStream      Weapon = 35
+	weaponLavaSpray       Weapon = 36
+	weaponSpikeGun        Weapon = 40
+	weaponSword           Weapon = 2
+	weaponBlinkDagger     Weapon = 4
+	weaponSpear           Weapon = 63
+	weaponTimeBubble      Weapon = 21
+	weaponLaser           Weapon = 14
+	weaponIceGun          Weapon = 7
+	weaponBlackHole       Weapon = 41
+	weaponGlueGun         Weapon = 27
+	weaponMinigun         Weapon = 19
+	weaponFlameThrower    Weapon = 23
 )
 
 //NetworkWeapon holds a player's current weapon according to the network
